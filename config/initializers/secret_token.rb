9b3fc8a4be7276248dedf9fa483d29ebf1cdfa38
Jack Pipitone <pipitone.jack@gmail.com>
2016-07-01 17:16:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todoster::Application.config.secret_key_base = '3ff92bc3eaa1b78ed2ff883408208e3fab4521c26d89f5da7d90b9725e7d86a6b5d0de1926a4444d942c26ed0484ed6804258c1f89b0414c3550f52bb0307506'
